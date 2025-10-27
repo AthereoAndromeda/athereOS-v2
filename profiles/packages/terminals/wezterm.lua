980e9093cb2cb3f51ae4817e024a8ddb25d61235
Athereo <athereoandromeda@gmail.com>
2025-10-27 19:49:51 +0800

    style(wezterm): Change opacity, padding, buttons

--- profiles/packages/terminals/wezterm.lua
+++ profiles/packages/terminals/wezterm.lua
@@ -2,8 +2,18 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha"
-config.window_decorations = "INTEGRATED_BUTTONS"
+-- config.window_decorations = "INTEGRATED_BUTTONS"
 config.enable_wayland = true
 config.integrated_title_button_style = "Gnome"
+
+config.window_background_opacity = 0.8
+
+config.window_padding = {
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
+}
+
 
 return config
