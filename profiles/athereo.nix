4bdbb0e463243f913904adb51e9f4962c8d1a1a1
Athereo <athereoandromeda@gmail.com>
2025-10-27 19:46:28 +0800

    fix: Disable termfilepicker temporarily

--- profiles/athereo.nix
+++ profiles/athereo.nix
@@ -21,30 +21,33 @@
 
   # TODO: fix XDG Portals
   xdg.portal.enable = true;
+  xdg.portal.xdgOpenUsePortal = true;
   xdg.portal.extraPortals = with pkgs; [
     xdg-desktop-portal-gnome
-    xdg-desktop-portal-termfilechooser
+    xdg-desktop-portal-gtk
+    # xdg-desktop-portal-termfilechooser
   ];
 
   xdg.portal.config = {
     common = {
       default = [
         "gnome"
+        "gtk"
       ];
 
       "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
     };
   };
 
-  services.xdg-desktop-portal-termfilepickers = let
-    termfilepickers = inputs.xdg-termfilepickers.packages.${pkgs.system}.default;
-  in {
-    enable = true;
-    package = termfilepickers;
-    config = {
-      terminal_command = [(lib.getExe pkgs.ghostty) "-e"];
-    };
-  };
+  # services.xdg-desktop-portal-termfilepickers = let
+  #   termfilepickers = inputs.xdg-termfilepickers.packages.${pkgs.system}.default;
+  # in {
+  #   enable = true;
+  #   package = termfilepickers;
+  #   config = {
+  #     terminal_command = [(lib.getExe pkgs.ghostty) "--title=filepicker" "-e"];
+  #   };
+  # };
 
   home.stateVersion = "25.05";
   home.homeDirectory = "/home/athereo";
