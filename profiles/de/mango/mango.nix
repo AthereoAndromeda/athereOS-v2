027c3eadac9e289e64f5133f863c9cdb2d59a7f5
Athereo <athereoandromeda@gmail.com>
2025-12-27 13:37:14 +0800

    refactor(mango): Modularize `mango` config

--- profiles/de/mango/mango.nix
+++ profiles/de/mango/mango.nix
@@ -1,19 +1,34 @@
-{...}: {
+{config, ...}: let
+  inherit (config.home.sessionVariables) XCURSOR_THEME XCURSOR_SIZE;
+in {
   imports = [
     ./wlsunset.nix
     ./wayidle/wayidle.nix
   ];
+
   wayland.windowManager.mango = {
     enable = true;
     # settings = ''
     #   # see config.conf
     # '';
-    settings = builtins.readFile ./mango.conf;
+    # settings = builtins.readFile ./mango.conf;
 
     # autostart_sh = ''
     #   # see autostart.sh
     #   # Note: here no need to add shebang
     #   wpaperd
     # '';
+  };
+
+  xdg.configFile = {
+    "mango" = {
+      recursive = true;
+      source = ./config;
+    };
+
+    "mango/cursors.conf".text = ''
+      cursor_theme=${XCURSOR_THEME}
+      cursor_size=${XCURSOR_SIZE}
+    '';
   };
 }
