db7ae3b59d67eb2ef871c3640f15cf9f779eed41
Athereo <athereoandromeda@gmail.com>
2025-12-29 18:23:33 +0800

    refactor(fonts): Refactor `fonts.nix`

--- nixos/packages/font/fonts.nix
+++ nixos/packages/font/fonts.nix
@@ -1,17 +1,24 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  install-font = path:
+    pkgs.runCommand "install-font" {} ''
+      mkdir -p $out/share/fonts/truetype
+
+      # -L follows symlinks if the directory is a Nix store path
+      # -iregex allows case-insensitive matching for various extensions
+      find -L ${path} -type f \( -iname "*.ttf" -o -iname "*.otf" -o -iname "*.woff" -o -iname "*.woff2" \) \
+        -exec cp -t $out/share/fonts/truetype/ {} +
+    '';
+in {
+  environment.systemPackages = [
+    pkgs.gnome-font-viewer
+  ];
+
   fonts.packages = with pkgs.nerd-fonts;
     [
       jetbrains-mono
       sauce-code-pro
     ]
     ++ [
-      (pkgs.runCommand "nuixyber-glow" {} ''
-        mkdir -p $out/share/fonts/truetype
-        cp ${./nuixyber-glow-font/NuixyberGlow-x3KP8.ttf} ${./nuixyber-glow-font/NuixyberGlowNext-3zWjZ.ttf} $out/share/fonts/truetype/
-      '')
-      (pkgs.runCommand "library-3am" {} ''
-        mkdir -p $out/share/fonts/truetype
-        cp ${./library-3-am-font/Library3am-5V3Z.otf} ${./library-3-am-font/Library3amsoft-6zgq.otf} $out/share/fonts/truetype/
-      '')
+      (install-font ./library-3-am-font)
     ];
 }
