dcaa8c4bd549ac18021a8f7b90660694defd6dd0
Athereo <athereoandromeda@gmail.com>
2026-01-16 09:52:42 +0800

    feat: Refactor and modify config

--- profiles/de/niri/niri.nix
+++ profiles/de/niri/niri.nix
@@ -1,5 +1,11 @@
 {pkgs, ...}: let
   inherit (builtins) readFile;
 in {
-  programs.niri.config = readFile ./default-config.kdl;
+  imports = [
+    ./quickshell
+  ];
+
+  programs.niri.config = readFile ./config.kdl;
+
+  programs.fuzzel.enable = true;
 }
