b773fd133baf9c0df3d9ee42344ffd8408cced27
Athereo <athereoandromeda@gmail.com>
2026-02-03 22:08:29 +0800

    fix: Revert back to rootful docker

--- nixos/virt/docker.nix
+++ nixos/virt/docker.nix
@@ -1,18 +1,22 @@
 {...}: {
   virtualisation.docker = {
-    # Consider disabling the system wide Docker daemon
-    enable = false;
-
+    enable = true;
     storageDriver = "btrfs";
-
-    rootless = {
-      enable = true;
-      setSocketVariable = true;
-      # Optionally customize rootless Docker daemon settings
-      # daemon.settings = {
-      #   dns = ["1.1.1.1" "8.8.8.8"];
-      #   registry-mirrors = ["https://mirror.gcr.io"];
-      # };
-    };
   };
+  # virtualisation.docker = {
+  #   # Consider disabling the system wide Docker daemon
+  #   enable = false;
+
+  #   storageDriver = "btrfs";
+
+  #   rootless = {
+  #     enable = true;
+  #     setSocketVariable = true;
+  #     # Optionally customize rootless Docker daemon settings
+  #     # daemon.settings = {
+  #     #   dns = ["1.1.1.1" "8.8.8.8"];
+  #     #   registry-mirrors = ["https://mirror.gcr.io"];
+  #     # };
+  #   };
+  # };
 }
